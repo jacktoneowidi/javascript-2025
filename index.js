7c8c22ce0aee7e0cafb2d388e8682d5d97a261cf
jacktone owidi <125829691+jacktoneowidi@users.noreply.github.com>
2025-06-22 19:08:08 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+// learnt about booleans anf if statements.
+ <script>
+        
+        
+        true 
+        false
+
+        const age = 15;
+
+        if (age >= 40) {
+            console.log('hello');
+        }
+        else{
+            console.log('else');
+        }
+
+
+        if(15>= 16){
+            console.log('you can drive' );
+
+        }
+        
+        else if(15 >= 14){
+            console.log('almost there')
+        }
+
+        else{
+            console.log ('you cant drive');
+        }
+
+        
+    </script>
