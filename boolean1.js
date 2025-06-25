2be86a7fbe2d38c16744407d0d7f48e45e6e68b5
jacktone owidi <125829691+jacktoneowidi@users.noreply.github.com>
2025-06-25 05:22:51 -0700

    Create boolean1.js

---
+++ boolean1.js
@@ -0,0 +1,12 @@
+// what i have learnt today 
+
+
+
+
+scipt
+  const randomNumber = Math.random();
+        if(randomNumber >= 0 && randomNumber 1 / 3 ){
+            console.log('rock');
+        }else if()
+        
+scipt
